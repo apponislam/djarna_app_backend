dc3a2e53ddc61ac73dd8af0e6eab0869cb448f7d
Appon Islam <appon2003@gmail.com>
2026-07-17 01:13:06 +0600

    define IReport interface and associated types for report module

--- src/app/modules/report/report.interface.ts
+++ src/app/modules/report/report.interface.ts
@@ -6,9 +6,9 @@ export type ReportStatus = "OPEN" | "IN_REVIEW" | "RESOLVED";
 export interface IReport {
     reportId: string;
     type: ReportType;
-    reportedItem: Types.ObjectId; // Could be Product ID or User ID
-    reporter: Types.ObjectId; // User who is reporting
-    reportedUser: Types.ObjectId; // User who owns the listing or is being reported
+    reportedItem: Types.ObjectId;
+    reporter: Types.ObjectId;
+    reportedUser: Types.ObjectId;
     reason: string;
     details?: string;
     status: ReportStatus;
